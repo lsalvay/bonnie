1e2a4b461fdd9f08fe23131f0b6bb4e6088906bc
Luis Pablo Salvay <lsalvay@gmail.com>
2014-12-05 16:21:27 -0300

    iniciando el proyecto con los archivos y carpetas basicos

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<?php include('head.php') ?>
+	 <title>Home</title>
+</head>
+<body>
+    <header>
+        <h1>Este es el home de Bonnie and Clyde</h1>
+    </header>
+    <nav>
+    </nav>
+    <section>
+    </section>
+</body>
+<?php include('scripts.php') ?>
+</html>
